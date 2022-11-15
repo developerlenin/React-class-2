76ff25a7e5e1b2b58c6649a49b63ea3be0e66530
developerlenin <justlenin9095@gmail.com>
2022-11-15 17:53:06 +0530

    new

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 var btn=document.getElementById("btn");
 var btnone=document.getElementById("btn-one");
-
     var obj=[
-        {id:1, Name:"Apple",   Price:30000, Model:2021},
-        {id:2, Name:"Redmi",   Price:20000, Model:2020},
-        {id:3, Nmae:"Samsung", Price:25000, Model:2022}
-    ]
-       var cart=[]; 
-        function cart(){
-            var prom= prompt("Id:");
-            let result = obj.find(function(obj){
-
-               return obj.id==prom;   
-            })
-            cart.push( result);
-            console( result);
-            document.getElementById("para").innerHTML=cart;
-        };
+        {id:1, Name:"Apple",   Price:30000, Model:2021,qty:1},
+        {id:2, Name:"Redmi",   Price:20000, Model:2020,qty:1},
+        {id:3, Nmae:"Samsung", Price:25000, Model:2022,qty:1}
+    ];
+ 
+    var prom;         
+      var cartitem=[];
+      
+       function cart(){
+        prom=prompt("Id");
+        var result=obj.find(function(item,){
+            return item.id==prom;
+        });
+        cartitem.push(result);
+        console.log(result)
+       };
        
+       function cartitems(){
+        var res=[];
+        for(var i=0;i<cartitem.length;i++){
+         res.push(cartitem[i]);
+        console.log('test',prom);
+        };
+            
+            var temp=res.map(function(item){
+                console.log(item);
+                if(prom == item.id){
+                    item.qty++;
+                }
+                
+                return res;  
+            })
+    
+        
+        document.getElementById("para").innerText=JSON.stringify(res);
+       };
+      
+       
+    
+
+    
